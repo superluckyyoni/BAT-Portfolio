d65ab6a85c855bacd6530e43cc4b0d35c059bfa8
superluckyyoni <returnpride@gmail.com>
2026-05-11 21:17:20 +0900

    conftest fix

--- conftest.py
+++ conftest.py
@@ -20,3 +20,9 @@ def pytest_sessionfinish(session, exitstatus):
         msg = f"❌ BAT 결과: {passed}개 통과 / {failed}개 실패! 빌드 확인 필요!"
 
     send_discord(msg)
+
+def pytest_runtest_logreport(report):
+    """각 테스트 실패 시 즉시 디스코드로 알림"""
+    if report.when == "call" and report.failed:
+        msg = f"🔴 실패 항목: {report.nodeid}"
+        send_discord(msg)
